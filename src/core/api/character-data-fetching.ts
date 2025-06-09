6d07d76a3ac14ca09e5f06dd6c616bcf668dfd46
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-06-09 13:57:28 -0300

    chore: remove console logs

--- src/core/api/character-data-fetching.ts
+++ src/core/api/character-data-fetching.ts
@@ -13,7 +13,6 @@ const fetchCharacter = async (name: CharacterName): Promise<JSON | null> => {
 
 const fetchedDataToCharacter = (fetchedData: JSON): Character => {
   const data = JSON.parse(JSON.stringify(fetchedData));
-  console.log("data: ", data);
   return {
     name: data.query.pages[0].title,
     img: data.query.pages[0].original.source,
@@ -23,7 +22,6 @@ const fetchedDataToCharacter = (fetchedData: JSON): Character => {
 export const getCharacterData = async (
   name: CharacterName,
 ): Promise<Character | null> => {
-  console.log(name);
   const fetchedData = await fetchCharacter(name);
   if (!fetchedData) {
     console.error("there were problems fetching the data");
